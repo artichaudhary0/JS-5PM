c7d5edbaf873421fdb2a6a92e7cb4d4cfae466e2
artichaudhary0 <chaudhary.aarti1998@gmail.com>
2025-02-10 17:58:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+/* 
+
+    1 : javascript : HLL (High level programming language)
+    2 : helps creating dynamic components.
+    3 : HTML CSS manipulate / integrate javascript.
+    4 : client-side scripting language now its also working at server side(expand).
+    5 : server side : node.js
+
+
+    Es5 - Es6 : 
+    ECMScript(ES) :
+                            ES5                       ES6
+    1 : variable           : var                       const let var
+    2 : function           : Traditional               allow function (=>)
+    3 : classes            : prototype based           OOPS concept
+    4 : Template literals  : "Arti " + "Chaudhary"     `Arti {chaudhary}`
+    5 : defaut parameter   : (int num)                 num
+    6 : spread operator    : not applicable            ...
+    7 : Module             : Builtin not present       import and export 
+    8 : Promises           : async                     await/async
+    
+*/
